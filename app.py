42fa9582546bddfe495a6426c858a6386482fe50
Parnian Malekian <100802204+parnian91@users.noreply.github.com>
2024-01-03 15:47:56 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!pip install streamlit
+
+
+# In[2]:
+
+
+#!pip install --upgrade altair
+
+
+# In[3]:
+
+
+#!pip install pandas
+
+
+# In[4]:
+
+
+# import all libraries needed
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+import warnings
+# To ignore all warnings
+warnings.filterwarnings("ignore")
+
+from absenteeism_module import *
+
+
+# In[5]:
+
+
+def Input_Output():
+    data = st.file_uploader('ipload file', type={'csv', 'txt'})
+    if data is not None:
+        df = pd.read_csv(data)
+        model = absenteeism_model(r'C:\Users\98918\Desktop\Employee absenteeism\model')
+        model.load_and_clean_data(r'C:\Users\98918\Desktop\Employee absenteeism\Absenteeism_new_data.csv')
+    result = ''
+    if st.button('Click here to Predict'):
+        result = model.predicted_outputs()
+        st.balloons()
+    st.success('The output is as follows: ')
+    st.write(result)
+
+if __name__ =='__main__':
+    Input_Output()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
